bfd47e504c2980c710f91fb1aecd6dbbbf5386d6
LukeS26 <thewarriorls@outlook.com>
2019-11-14 15:15:40 -0500

    fixed some minor formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import  com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
