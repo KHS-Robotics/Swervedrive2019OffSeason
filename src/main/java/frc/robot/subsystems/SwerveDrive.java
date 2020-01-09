176f8880db41b5bb841f73a06938cfb0ff1a9100
LukeS26 <thewarriorls@outlook.com>
2020-01-09 15:38:07 -0500

    Added swerve logic

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -22,8 +22,9 @@ public class SwerveDrive extends Subsystem {
   // here. Call these from Commands.
 
   public SwerveModule swerveModuleFrontRight, swerveModuleFrontLeft, swerveModuleRearRight, swerveModuleRearLeft;
-  private final double l = 25.75, w = 21, r = Math.sqrt((l * l) + (w * w)), L_OVER_R = l / r, W_OVER_R = w / r, MIN_X = 0.05, MIN_Y = 0.075, MIN_Z = 0.05;
-  private double a, b, c, d, speed;
+  private final double l = 25.75, w = 21, r = Math.sqrt((l * l) + (w * w)), L_OVER_R = l / r, W_OVER_R = w / r,
+      MIN_X = 0.05, MIN_Y = 0.075, MIN_Z = 0.05;
+  private double a, b, c, d;
   private boolean fieldOriented;
 
   @Override
@@ -45,73 +46,96 @@ public class SwerveDrive extends Subsystem {
 
   public void setFOD(boolean fod) {
     fieldOriented = fod;
-  } 
+  }
 
   public void set(double x, double y, double z) {
-    //Setup
+    // Setup
 
     y = -y;
     // Sets front to actual front
 
-    if(Math.abs(x) < MIN_X && Math.abs(y) < MIN_Y && Math.abs(z) < MIN_Z) {
-      swerveModuleFrontLeft.set(0, swerveModuleFrontLeft.getAngle());
-      swerveModuleFrontRight.set(0, swerveModuleFrontRight.getAngle());
-      swerveModuleRearRight.set(0, swerveModuleRearRight.getAngle());
-      swerveModuleRearLeft.set(0, swerveModuleRearLeft.getAngle());
-    } else {
-      if(fieldOriented) {
-        double angle = Robot.navx.getAngle();
-        double temp = y * Math.cos(angle) + x * Math.sin(angle);
-        x = -y * Math.sin(angle) + x * Math.cos(angle);
-        y = temp;
-      }
-
-      a = x - (z * L_OVER_R);
-      b = x + (z * L_OVER_R);
-      c = y - (z * W_OVER_R);
-      d = y + (z * W_OVER_R);
-
-      SmartDashboard.putNumber("A", a);
-      SmartDashboard.putNumber("B", b);
-      SmartDashboard.putNumber("C", c);
-      SmartDashboard.putNumber("D", d);
-
-      //Motor 1 (b,c)
-      swerveModuleFrontRight.setPivot(Math.atan2(b,c) * 180/Math.PI + 180);
-      speed = Math.sqrt(b*b + c*c);
-
-      speed = speed > 1 ? 1 : speed;
-      speed = speed < -1 ? -1 : speed;
-
-      swerveModuleFrontRight.setDrive(speed);
-
-      //Motor 2 (b,d)
-      swerveModuleFrontLeft.setPivot(Math.atan2(b,d) * 180/Math.PI + 180);
-      speed = Math.sqrt(b*b + d*d);
-
-      speed = speed > 1 ? 1 : speed;
-      speed = speed < -1 ? -1 : speed;
-
-      swerveModuleFrontLeft.setDrive(speed);
-
-      //Motor 3 (a,c)
-      swerveModuleRearRight.setPivot(Math.atan2(a,c) * 180/Math.PI + 180);
-      speed = Math.sqrt(a*a + d*d);
-
-      speed = speed > 1 ? 1 : speed;
-      speed = speed < -1 ? -1 : speed;
-
-      swerveModuleRearRight.setDrive(speed);
-
-      //Motor 4 (a,c)
-      swerveModuleRearLeft.setPivot(Math.atan2(a,d) * 180/Math.PI + 180);
-      speed = Math.sqrt(a*a + c*c);
-
-      speed = speed > 1 ? 1 : speed;
-      speed = speed < -1 ? -1 : speed;
-
-      swerveModuleRearLeft.setDrive(speed);
+    if (fieldOriented) {
+      double angle = Robot.navx.getAngle();
+      double temp = y * Math.cos(angle) + x * Math.sin(angle);
+      x = -y * Math.sin(angle) + x * Math.cos(angle);
+      y = temp;
     }
+
+    a = x - (z * L_OVER_R);
+    b = x + (z * L_OVER_R);
+    c = y - (z * W_OVER_R);
+    d = y + (z * W_OVER_R);
+
+    SmartDashboard.putNumber("A", a);
+    SmartDashboard.putNumber("B", b);
+    SmartDashboard.putNumber("C", c);
+    SmartDashboard.putNumber("D", d);
+
+    // if (Math.abs(x) < MIN_X && Math.abs(y) < MIN_Y && Math.abs(z) < MIN_Z) {
+    // swerveModuleFrontLeft.set(0, swerveModuleFrontLeft.getAngle());
+    // swerveModuleFrontRight.set(0, swerveModuleFrontRight.getAngle());
+    // swerveModuleRearRight.set(0, swerveModuleRearRight.getAngle());
+    // swerveModuleRearLeft.set(0, swerveModuleRearLeft.getAngle());
+
+    // Motor 1 (b,c)
+    double frPivot = Math.atan2(b, c) * 180 / Math.PI + 180;
+    double frSpeed = Math.sqrt(b * b + c * c);
+    
+    // Motor 2 (b,d)
+    double flPivot = Math.atan2(b, d) * 180 / Math.PI + 180;
+    double flSpeed = Math.sqrt(b * b + d * d);
+
+    // Motor 3 (a,c)
+    double rrPivot = Math.atan2(a, c) * 180 / Math.PI + 180;
+    double rrSpeed = Math.sqrt(a * a + d * d);
+
+    // Motor 4 (a,c)
+    double rlPivot = Math.atan2(a, d) * 180 / Math.PI + 180;
+    double rlSpeed = Math.sqrt(a * a + c * c);
+
+    if ((a == 0 && Math.abs(y) < 0.005) || (Math.abs(x) < 0.005 && Math.abs(y) < 0.005 && Math.abs(z) < 0.005)) {
+      frPivot = swerveModuleFrontRight.getAngle();
+      flPivot = swerveModuleFrontLeft.getAngle();
+      rrPivot = swerveModuleRearRight.getAngle();
+      rlPivot = swerveModuleRearLeft.getAngle();
+    } else if (a == 0) {
+      if (y > 0.02) {
+        frPivot = flPivot = rrPivot = rlPivot = 180;
+      } else if (y < -0.15) {
+        frPivot = flPivot = rrPivot = rlPivot = 0;
+      } else {
+        frPivot = swerveModuleFrontRight.getAngle();
+        flPivot = swerveModuleFrontLeft.getAngle();
+        rrPivot = swerveModuleRearRight.getAngle();
+        rlPivot = swerveModuleRearLeft.getAngle();
+      }
+    }
+
+    double max = Math.abs(frSpeed);
+		if(Math.abs(flSpeed) > max)
+			max = Math.abs(flSpeed);
+		if(Math.abs(rlSpeed) > max)
+			max = Math.abs(rlSpeed);
+		if(Math.abs(rrSpeed) > max)
+			max = Math.abs(rrSpeed);
+		
+		if(max > 1) {
+			frSpeed /= max;
+			flSpeed /= max;
+			rlSpeed /= max;
+			rrSpeed /= max;
+    }
+    
+    swerveModuleFrontRight.setDrive(frSpeed);
+    swerveModuleFrontLeft.setDrive(flSpeed);
+    swerveModuleRearRight.setDrive(rrSpeed);
+    swerveModuleRearLeft.setDrive(rlSpeed);
+
+    swerveModuleFrontRight.setPivot(frPivot);
+    swerveModuleFrontLeft.setPivot(flPivot);
+    swerveModuleRearRight.setPivot(rrPivot);
+    swerveModuleRearLeft.setPivot(rlPivot);
+
   }
 
   public void stop() {
