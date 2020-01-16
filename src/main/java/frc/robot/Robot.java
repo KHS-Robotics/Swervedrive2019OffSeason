ecdfa3006e23c71ea911f179b3c0c277dd05c327
kjelstromjr <jeffreykjelstrom@gmail.com>
2020-01-16 15:34:32 -0500

    Added Pixy Camera Color Detecting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,10 +17,13 @@ import com.kauailabs.navx.frc.AHRS;
 import frc.robot.logging.Logger;
 import frc.robot.subsystems.SwerveDrive;
 import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
-import edu.wpi.first.wpilibj.AnalogInput; 
+import edu.wpi.first.wpilibj.AnalogInput;
 
-import  com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import java.util.ArrayList;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -36,11 +39,9 @@ public class Robot extends TimedRobot {
   public static AHRS navx;
   public static Pixy2 pixy;
 
-  
-
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
 
   @Override
@@ -54,54 +55,85 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
 
   @Override
   public void robotPeriodic() {
-    
+
   }
 
   /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
+   * This function is called once each time the robot enters Disabled mode. You
+   * can use it to reset any subsystem information you want to clear when the
+   * robot is disabled.
    */
 
   @Override
   public void disabledInit() {
-    pixy.setLamp((byte) 0, (byte) 0);
+    // pixy.setLamp((byte) 0, (byte) 0);
   }
+
+  int counter = 0;
 
   @Override
   public void disabledPeriodic() {
-    Scheduler.getInstance().run();
+    pixy.getCCC().getBlocks(true, 255, 2);
+    ArrayList<Block> blocks = pixy.getCCC().getBlocks();
+    // if(counter > 6) {
+    // for (Block block : blocks) {
+    // System.out.println(block);
+    // }
+    // System.out.println("------------");
+    // counter = 0;
+    // }
+    // counter++;
+    if (blocks.size() > 1) {
+      System.out.println("Saw more than one block");
+      System.out.println(blocks.toString());
+    }
+
+    SmartDashboard.putBoolean("Yellow", false);
+    SmartDashboard.putBoolean("Red", false);
+    SmartDashboard.putBoolean("Green", false);
+    SmartDashboard.putBoolean("Blue", false);
+
+    for (Block block : blocks) {
+      ColorWheel color = ColorWheel.toColor(block.getSignature());
+      
+      SmartDashboard.putBoolean("Yellow", color.isYellow() ? true : SmartDashboard.getBoolean("Yellow", false));
+      SmartDashboard.putBoolean("Red", color.isRed() ? true : SmartDashboard.getBoolean("Red", false));
+      SmartDashboard.putBoolean("Green", color.isGreen() ? true : SmartDashboard.getBoolean("Green", false));
+      SmartDashboard.putBoolean("Blue", color.isBlue() ? true : SmartDashboard.getBoolean("Blue", false));
+    }
   }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString code to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
+   * <p>
+   * You can add additional auto modes by adding additional commands to the
+   * chooser code above (like the commented example) or additional comparisons to
+   * the switch structure below with additional strings & commands.
    */
 
   @Override
   public void autonomousInit() {
     /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
+     * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+     * switch(autoSelected) { case "My Auto": autonomousCommand = new
+     * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+     * ExampleCommand(); break; }
      */
   }
 
@@ -116,9 +148,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    //Scheduler.getInstance().add(new PivotPIDTuner(swerveDrive.swerveModuleRearRight));
-    pixy.setLamp((byte) 1, (byte) 1); // Turns the LEDs on
-		//pixy.setLED(200, 30, 255); // Sets the RGB LED to purple
+    // Scheduler.getInstance().add(new
+    // PivotPIDTuner(swerveDrive.swerveModuleRearRight));
+    // pixy.setLamp((byte) 1, (byte) 1); // Turns the LEDs on
+    // pixy.setLED(200, 30, 255); // Sets the RGB LED to purple
   }
 
   /**
@@ -133,7 +166,7 @@ public class Robot extends TimedRobot {
   /**
    * This function is called periodically during test mode.
    */
-  
+
   @Override
   public void testPeriodic() {
   }
