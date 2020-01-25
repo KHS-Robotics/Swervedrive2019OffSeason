2a569f132d11b575585b2b1a956e330adb7b1e18
kjelstromjr <jeffreykjelstrom@gmail.com>
2020-01-25 16:49:47 -0500

    Added start of RotateToAngleInPlace

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Limelight.LightMode;
 import frc.robot.commands.DriveSwerveWithXbox;
 
 public class Robot extends TimedRobot {
@@ -16,6 +17,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     RobotContainer.swerveDrive.setDefaultCommand(new DriveSwerveWithXbox());
+    Limelight.setLedMode(LightMode.eOff);
   }
 
   @Override
